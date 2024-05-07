a8733a83d09ed09aa5a16186c740d0bd38060fa1
laopaovip <161992182+laopaovip@users.noreply.github.com>
2024-05-07 17:37:14 +0800

    Add files via upload
    new deploy.js

--- taiko-hekla/scripts/deploy.js
+++ taiko-hekla/scripts/deploy.js
@@ -1,1 +1,21 @@
+const { ethers } = require("hardhat");
 
+async function main() {
+  const StorageContract = await ethers.getContractFactory("Storage");
+  const storageContract = await StorageContract.deploy();
+
+  await storageContract.waitForDeployment();
+  const tx = await storageContract.deploymentTransaction();
+
+  console.log("Contract deployed successfully.");
+  console.log(`Deployer: ${storageContract.runner.address}`);
+  console.log(`Deployed to: ${storageContract.target}`);
+  console.log(`Transaction hash: ${tx.hash}`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
